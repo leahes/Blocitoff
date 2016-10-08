4b34e46bcf1b6c5dd6095359727178c0e1d64e42
Leah <leahstuckey@me.com>
2016-10-08 14:25:42 -0500

    wip98

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :only => [:show]
-  get 'users/show'
-  root to: 'welcome#home'
+  resources :users, only: [:show]
+  root to: 'users#show'
 end
