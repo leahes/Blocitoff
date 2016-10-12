4c91237cb5314e552195f87660855876f47178ad
Leah <leahstuckey@me.com>
2016-10-11 19:51:03 -0500

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show] do
-    resources :items, only: [:create]
+
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
   end
 
-  root to: 'users#show'
+  root "welcome#home"
 end
+#resources :items, only: [:create]
