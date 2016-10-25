595e63b548694520bda2e3d40df0b1826a85711a
Leah <leahstuckey@me.com>
2016-10-24 19:12:13 -0500

    wip

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  #validates :name, presence: true
-  #validates :time_left, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :time_left, presence: true, numericality: { only_integer: true }
 
-  #def time_left
-  #self.time_left - (DateTime.now.to_date - created_at.to_date).to_item
-  #end
+  def time_left
+  self.time_left - (DateTime.now.to_date - created_at.to_date).to_item
+  end
 end
